34588c37c7bb8d5b7b803627280b72f7c8c69648
Borodkin <borodkinviktor@gmail.com>
2022-10-29 18:06:07 +0300

    Домашняя работа

---
+++ Задача-2/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+645 -> 5
+78 - > третьей цифры нет
+32679 -> 6 */
+
+Console.WriteLine("ВВедите трехзначное число");
+string ThreeNumber = Console.ReadLine();
+
+if(ThreeNumber.Length > 2)
+{
+    Console.WriteLine($"Третья цифра = {ThreeNumber[2]}");
+}
+else
+{
+    Console.WriteLine($"Третьей цифры нет");
+
+}
+
---
+++ Задача-3/Program.cs
@@ -0,0 +1,17 @@
+﻿ /*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую 
+день недели и проверяет, является ли этот день выходным
+6 -> да
+7 -> да
+1 -> нет */
+
+Console.WriteLine("ВВедите день недели, от 1 - 7");
+int number = int.Parse(Console.ReadLine());
+
+if(number >= 1 && number <= 5 )
+   Console.WriteLine("Рабочий день");
+else if(number == 6 || number ==7)
+   Console.WriteLine("Выходной день");
+else
+   Console.WriteLine("ВВедите день недели от 1-7");
+
+
